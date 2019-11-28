2d5a81c871b0299df9de1e69a44a71913365d27e
Jorrit Salverda <jsalverda@travix.com>
2019-11-28 18:06:24 +0100

    updated estafette formula

--- Formula/estafette.rb
+++ Formula/estafette.rb
@@ -2,35 +2,34 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Estafette < Formula
-    desc "The CLI for Estafette"
-    homepage ""
-    url "https://github.com/estafette/estafette/releases/download/v0.1.0-master-6/estafette-v0.1.0-master-6-darwin-amd64.zip"
-    version "v0.1.0-master-6"
-    sha256 "960e083130c46d6b22591076b0f37f292734f01f4e1e69637466ddf2f7c82ce1"
-  
-    # depends_on "cmake" => :build
-  
-    def install
-      # ENV.deparallelize  # if your formula fails when building in parallel
-      # Remove unrecognized options if warned by configure
-      system "./configure", "--disable-debug",
-                            "--disable-dependency-tracking",
-                            "--disable-silent-rules",
-                            "--prefix=#{prefix}"
-      # system "cmake", ".", *std_cmake_args
-    end
-  
-    test do
-      # `test do` will create, run in and delete a temporary directory.
-      #
-      # This test will fail and we won't accept that! For Homebrew/homebrew-core
-      # this will need to be a test that verifies the functionality of the
-      # software. Run the test with `brew test estafette`. Options passed
-      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-      #
-      # The installed folder is not in the path, so use the entire path to any
-      # executables being tested: `system "#{bin}/program", "do", "something"`.
-      system "false"
-    end
+  desc "Estafette CLI"
+  homepage "https://estafette.io"
+  url "https://github.com/estafette/estafette/releases/download/v0.1.0-master-6/estafette-v0.1.0-master-6-darwin-amd64.zip"
+  version "v0.1.0-master-6"
+  sha256 "9a9dcaf847ab547d4686b1b6edb5cc7f4e5b6f1f420fe7ba63f2299ff874532f"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
   end
-  
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test estafette`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
